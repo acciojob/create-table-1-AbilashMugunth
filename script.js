8fd06abfbb2fb760290286f9b17a838eecd58fe2
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-08 13:52:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let table = document.querySelector("#sampleTable");
-
+let count=0
 function insert_Row() {
   let td1 = document.createElement("td");
   let td2 = document.createElement("td");
-  td1.innerText = "New Cell1";
-  td2.innerText = "New Cell2";
+  td1.innerText = `New Cell${++count}`;
+  td2.innerText = `New Cell${++count}`;
 
   let tr = document.createElement("tr");
   tr.append(td1, td2);
