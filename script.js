f85cb0dd3b8eacf339db4f4f433d325ef02fd070
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-08 13:46:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+let table = document.querySelector("#sampleTable");
+
 function insert_Row() {
-    //Write your code here
-  
-  
+  let td1 = document.createElement("td");
+  let td2 = document.createElement("td");
+  td1.innerText = "New Cell1";
+  td2.innerText = "New Cell2";
+
+  let tr = document.createElement("tr");
+  tr.append(td1, td2);
+
+  table.insertAdjacentElement("afterbegin", tr);
 }
